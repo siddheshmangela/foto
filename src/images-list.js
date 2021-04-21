8ca2a78adf9f688c9f17a6015e025b9e910320cb
Siddhesh Mangela <sidthecool007@gmail.com>
2021-04-21 16:27:47 +0530

    small sized image added with chroma sampled

--- src/images-list.js
+++ src/images-list.js
@@ -1,12 +1,12 @@
-import backRoomAjanta from './images/DSC_0154-tagged.jpg';
-import walkTheWalk from './images/DSC_0550-edit-c.jpg';
-import pyramid from './images/DSC_0108-Edit-tagged.jpg';
-import manInAby from './images/DSC_0225-edit.jpg';
-import gallary from './images/DSC_0642-Edit-edit.jpg';
-import washOrVanish from './images/DSC_1745-Edit-edit-c.jpg';
-import angkorHdr from './images/DSC_7559-HDR-tagged.jpg';
-import gateOfTheDeadBargin from './images/DSC_7876-Pano-Edit-Edit-edit-tagged.jpg';
-import watBuddha from './images/DSC_8220-tagged.jpg';
+import backRoomAjanta from './images/DSC_0154-tagged-202104162011.jpg';
+import walkTheWalk from './images/DSC_0550-edit-c-202104162011.jpg';
+import pyramid from './images/DSC_0108-Edit-tagged-202104162011.jpg';
+import manInAby from './images/DSC_0225-edit-202104162011.jpg';
+import gallary from './images/DSC_0642-Edit-edit-202104162011.jpg';
+import washOrVanish from './images/DSC_1745-Edit-edit-c-202104162011.jpg';
+import angkorHdr from './images/DSC_7559-HDR-tagged-202104162011.jpg';
+import gateOfTheDeadBargin from './images/DSC_7876-Pano-Edit-Edit-edit-tagged-202104162011.jpg';
+import watBuddha from './images/DSC_8220-tagged-202104162011.jpg';
 
 export default [
   {
