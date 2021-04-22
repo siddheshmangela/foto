3fe929d2dc0f15a6fea7e1dc7235206c69ffb29c
Siddhesh Mangela <sidthecool007@gmail.com>
2021-04-22 11:15:02 +0530

    images alt added

--- src/images-list.js
+++ src/images-list.js
@@ -1,48 +1,49 @@
-import backRoomAjanta from './images/DSC_0154-tagged-202104162011.jpg';
-import walkTheWalk from './images/DSC_0550-edit-c-202104162011.jpg';
-import pyramid from './images/DSC_0108-Edit-tagged-202104162011.jpg';
-import manInAby from './images/DSC_0225-edit-202104162011.jpg';
-import gallary from './images/DSC_0642-Edit-edit-202104162011.jpg';
-import washOrVanish from './images/DSC_1745-Edit-edit-c-202104162011.jpg';
-import angkorHdr from './images/DSC_7559-HDR-tagged-202104162011.jpg';
-import gateOfTheDeadBargin from './images/DSC_7876-Pano-Edit-Edit-edit-tagged-202104162011.jpg';
-import watBuddha from './images/DSC_8220-tagged-202104162011.jpg';
+import backRoomAjanta from './images/DSC_0154-tagged.jpg';
+import walkTheWalk from './images/DSC_0550-edit-c.jpg';
+import pyramid from './images/DSC_0108-Edit-tagged.jpg';
+import manInAby from './images/DSC_0225-edit.jpg';
+import gallary from './images/DSC_0642-Edit-edit.jpg';
+import washOrVanish from './images/DSC_1745-Edit-edit-c.jpg';
+import angkorHdr from './images/DSC_7559-HDR-tagged.jpg';
+import gateOfTheDeadBargin from './images/DSC_7876-Pano-Edit-Edit-edit-tagged.jpg';
+import watBuddha from './images/DSC_8220-tagged.jpg';
 
 export default [
   {
-    name: 'backRoom Ajanta',
+    name:
+      'Archaeological restoration of wall paintings, Ajanta Caves, Ajanta, Maharashtra',
     image: backRoomAjanta,
   },
   {
-    name: 'Gate Of The Dead Bargin',
+    name: 'Gate Of The Dead, Angkor Thom, Siem Reap',
     image: gateOfTheDeadBargin,
   },
   {
-    name: 'man In Aby',
+    name: 'Man in amber light, Ajanta Caves, Ajanta, Maharashtra',
     image: manInAby,
   },
   {
-    name: 'washOrVanish',
+    name: 'Half cut Shadow of a man',
     image: washOrVanish,
   },
   {
-    name: 'pyramid',
+    name: 'Pyramid of Khafre, Giza Necropolis',
     image: pyramid,
   },
   {
-    name: 'walk The Walk',
+    name: 'A man taking an evening stroll',
     image: walkTheWalk,
   },
   {
-    name: 'Wat Buddha',
+    name: 'Wat Preah Prom Rath, Siem Reap',
     image: watBuddha,
   },
   {
-    name: 'gallary',
+    name: 'Kailasa Temple, Ellora Caves, Verul, Maharashtra',
     image: gallary,
   },
   {
-    name: 'angkor HDR',
+    name: 'Sunrise at Angkor Wat, Siem Reap',
     image: angkorHdr,
   },
 ];
