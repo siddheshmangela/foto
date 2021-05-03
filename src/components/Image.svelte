ab9b7d1ebc45e34d422fc05dc2365db6a1c5c180
Siddhesh Mangela <sidthecool007@gmail.com>
2021-05-03 19:21:56 +0530

    statically added

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,24 +1,19 @@
 <script>
-  import { publicPath } from '../utils';
+  import { isDev } from '../utils';
   export let image;
 
-  let imageSrc = image.image;
+  const getImageUrl = () => {
+    // if (isDev) {
+    //   return `/images/${image.image}`;
+    // }
 
-  const handleAfterImageLoad = () => {
-    const filenameArray = image.image.split('/');
-    const fileName = filenameArray[filenameArray.length - 1];
-
-    imageSrc = `${publicPath}/images/${fileName}`;
+    return `https://cdn.statically.io/img/siddheshmangela.github.io/f=auto/foto/images/${image.image}`;
   };
+
+  const imageSrc = getImageUrl();
 </script>
 
-<img
-  src={imageSrc}
-  alt={image.name}
-  loading="lazy"
-  class="image"
-  on:load={handleAfterImageLoad}
-/>
+<img src={imageSrc} alt={image.name} loading="lazy" class="image" />
 
 <style>
   .image {
@@ -33,12 +28,14 @@
     image-rendering: -webkit-optimize-contrast; /* Webkit (non-standard naming) */
     image-rendering: crisp-edges;
     -ms-interpolation-mode: nearest-neighbor; /* IE (non-standard property) */
+    min-height: 200px;
   }
 
   @media only screen and (min-width: 1100px) {
     .image {
       max-width: 930px;
       margin-bottom: var(--unit-5);
+      min-height: 450px;
     }
 
     .image:first-child {
